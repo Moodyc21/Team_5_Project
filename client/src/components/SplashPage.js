5d26cd85ea4e08109f79e0404a0e70bc333d7d01
babylee2002 <babylee2002@gmail.com>
2018-02-12 14:54:27 -0500

    styling continues

--- client/src/components/SplashPage.js
+++ client/src/components/SplashPage.js
@@ -2,7 +2,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {push} from 'react-router-redux'
-import styled from ' styled components'
+import styled from 'styled-components'
 
 
 class SplashPage extends Component {
