f6bc543e3e4d6e2d54189668ffb8eaf954360a32
Brocier <jschoe04@gmail.com>
2018-02-13 10:52:28 -0500

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+  resources :cities do
+    resources :posts
+  end
+  resources :users do
+    resources :posts do
 end
