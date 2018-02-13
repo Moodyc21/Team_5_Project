96c39aaca55d1c9935212ef565426235d3cfc9ad
Christopher Moody <moodyc21@gmail.com>
2018-02-13 16:10:06 -0500

    'created post components'

---
+++ client/src/components/posts/NewPostForm.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {push} from 'react-router-redux'
+
+import {sendNewPostToDatabase} from '../../actions/thunk.actions.js'
+
+class PostForm extends Component {
+  state = {
+    newPostForm: {}
+  }
+
+  handleNewPostChange = (event) => {
+    const attributeName = event.target.name
+    const attributeValue = event.target.value
+
+    const newPostForm = {
+      ...this.state.newPostForm
+    }
+    newPostForm[attributeName] = attributeValue
+
+    this.setState({newPostForm})
+  };
+
+  handleAddNewPost = () => {
+    this
+      .props
+      .sendNewPostToDatabase(this.state.newPostForm)
+    this
+      .props
+      .push(`/posts`)
+    this.setState({
+      newPostForm: {
+        title: "",
+        content: ""
+      }
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          className="newPost"
+          type="text"
+          name="title"
+          onChange={this.handleNewPostChange}
+          value={this.state.newPostForm.title}
+          placeholder="Title"/>
+        <br/>
+        <input
+          className="newPost"
+          type="text"
+          name="content"
+          onChange={this.handleNewPostChange}
+          value={this.state.newPostForm.content}
+          placeholder="Content"/>
+        <br/>
+
+        <button onClick={this.handleAddNewPost}>
+          Add Post
+        </button>
+      </div>
+    )
+  }
+}
+
+export default connect(null, {sendNewPostToDatabase, push})(PostForm)
