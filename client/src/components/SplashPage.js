0bdd2541c241a2ebde6893e34436a21131a54ffb
babylee2002 <babylee2002@gmail.com>
2018-02-12 21:16:51 -0500

    project title changed

--- client/src/components/SplashPage.js
+++ client/src/components/SplashPage.js
@@ -8,7 +8,7 @@ class SplashPage extends Component {
   render() {
     return (
       <Container>
-        <h1>Vagabond</h1>
+        <h1>City Point</h1>
 
         <button type='button' onClick={() => this.props.push('/users')}>
           Enter
@@ -28,31 +28,34 @@ const Container = styled.div `
     height:100vh;
     top:0;
     left:0;
-    padding: 40px;
     right:0;
     bottom:0;
     font-family: 'Montserrat', sans-serif;
-    font-size: 30px;
+    color:#000033;
+    font-size: 45px;
     text-shadow:4px 4px 8px rgba(0,0,0,0.35);
     background-size: cover;
     background-repeat:no-repeat;
     background-image:linear-gradient(white,transparent,transparent,transparent,transparent),url(https://pbs.twimg.com/media/BzrxuvVIgAAj7YE.jpg:large);
+    h1{
+     position: absolute;
+     top:75px;
+    left:100px;
+    }
     button{
       border:1px solid black;
       background:none;
-      margin: 20px;
+      margin-left: 180px;
+      width:100px;
       padding:5px;
-      font-size: 30px;
+      font-size: 25px;
       font-family: 'Montserrat', sans-serif;
       border-radius: 5px;
+      position: absolute;
+      top:260px;
+      left:50px;
       cursor: pointer;
-    }
-    
-    /* a{
-        text-decoration: none;
-    color: white;
-    text-shadow:4px 4px 8px #000000;
-    z-index: auto;
-    
-    } */
-    `
+      &:hover{
+      background:rgba(0,0,0,0.15)
+      }
+    }`
