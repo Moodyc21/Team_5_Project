18871c27307850fa3b86d3525b53874da707045b
Christopher Moody <moodyc21@gmail.com>
2018-02-15 14:16:25 -0500

    created icons

--- client/src/components/SplashPage.js
+++ client/src/components/SplashPage.js
@@ -4,20 +4,22 @@ import {connect} from 'react-redux'
 import {push} from 'react-router-redux'
 import styled from 'styled-components'
 
-
 class SplashPage extends Component {
   render() {
     return (
-      
+
       <Container>
         <h1>City Point</h1>
         <div>
-        <button type='button' onClick={() => this.props.push('/users')}>
-          Users
-        </button>
-        <button type='button' onClick={() => this.props.push('/cities')}>
-          Cities
-        </button>
+          <Icons>
+            <div type='button' onClick={() => this.props.push('/users')}>
+              <img src="https://i.imgur.com/cOecPmi.png" alt="userLogo"/>
+            </div>
+
+            <div type='button' onClick={() => this.props.push('/cities')}>
+              <img src="https://i.imgur.com/0hOQIZS.png" alt="cityLogo"/>
+            </div>
+          </Icons>
         </div>
       </Container>
 
@@ -52,6 +54,17 @@ const Container = styled.div `
      top:65px;
      margin:20px;
     }
+    img{
+      width:10vh;
+      height:10vh;
+      cursor: pointer;
+      margin: 30px;
+      &:hover{
+        color:white;
+       opacity: 0.6;
+      transform:translateY(3px);
+      }
+    }
     button{
       border:1px solid black;
       background:none;
@@ -71,3 +84,10 @@ const Container = styled.div `
       transform:translateY(2px);
       }
     }`
+
+const Icons = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+`
